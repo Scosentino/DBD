bd79688f4fd36df5b7d8abecf2e86f60f2cd78cf
Sean Cosentino <Sparkcollaboration@gmail.com>
2015-08-17 17:22:47 -0400

    User resources

--- PortfolioRebuild/config/routes.rb
+++ PortfolioRebuild/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'users/new'
+
   root 'pages#homepage'
 
   get '/contact' => 'pages#contact'
@@ -8,6 +10,9 @@ Rails.application.routes.draw do
   get '/homepage' => 'pages#homepage'
 
   get 'portfolio' => 'pages#portfolio'
+
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
